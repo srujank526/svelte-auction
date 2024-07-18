9991ef8f1fc2642ae7fd694fa47f98417cc86e83
srujank526 <86570977+srujank526@users.noreply.github.com>
2024-07-18 16:40:47 +0530

    Update index.js

--- svelteclient/my-app/src/lib/index.js
+++ svelteclient/my-app/src/lib/index.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('localhost:8000');
+const socket = io('https://svelte-auction-backend.onrender.com/');
 // const socket = io('https://svelte-auction-backend-bvoownn1m-srujank526s-projects.vercel.app/');
 
 export default socket;
