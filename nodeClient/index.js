53bc935324c095f1125ca9244ae08a17bac4a71c
srujank526 <srujank526@gmail.com>
2024-07-18 10:02:53 +0530

    a

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -13,8 +13,7 @@ app.use('/',(req,res)=>{
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: '*localhost:5173', // Adjust this if necessary
-    methods: ["GET", "POST"],
+    origin: '*' // Adjust this if necessary
   }
 });
 
